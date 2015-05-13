99e3644f767294dd2dcccd13990fa42d2e6d0fd3
Calvin Scott <calvin.calvinscott.scott@gmail.com>
2015-05-12 18:08:14 -0700

    Update express-paramPamPam.js

--- expressworks/express-paramPamPam.js
+++ expressworks/express-paramPamPam.js
@@ -1,13 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.put('/message/:id', function(req, res){
-	var id = req.params.id
-	var hash = require('crypto')
-		.createHash('sha1')
-		.update(new Date().toDateString() + id)
-		.digest('hex')
-	res.send(hash)
-})
+	var id = req.params.id; // Took the longest to get this line...thanks YouTube!
+	var hash = require('crypto');
+		.createHash('sha1');
+		.update(new Date().toDateString() + id);
+		.digest('hex');
+	res.send(hash);
+});
 
-app.listen(process.argv[2])
+app.listen(process.argv[2]);
