4527a96928b0dd49e84e5697c0c7a8e631778ae6
Calvin Scott <calvin.calvinscott.scott@gmail.com>
2015-05-12 18:06:43 -0700

    Update express-jade.js

--- expressworks/express-jade.js
+++ expressworks/express-jade.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('views', process.argv[3]);
+app.set('views', process.argv[3]); // What exactly does this do? 'Views' request and command line function?
+
 app.set('view engine', 'jade');
+
 app.get('/', function(req, res){
 	res.render('index', {date: new Date().toDateString()});
-})
+});
+
 app.listen(process.argv[2]);
